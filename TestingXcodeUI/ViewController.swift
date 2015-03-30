eebd351e6350bdbbd9eb0cb20ef1f32f87676fee
Vitor Spencer <vitorspencer@gmail.com>
2015-03-30 11:30:54 +0200

    added second label

--- TestingXcodeUI/ViewController.swift
+++ TestingXcodeUI/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var mailButton: UIButton!
     
@@ -28,13 +29,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //unhide labels, set their texts and colours
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
         
-        // Clean message textfield entry and hide keyboard
+        // Clean textfield entries and hide keyboard
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
